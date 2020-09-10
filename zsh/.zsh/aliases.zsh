543270c2faa99c23f4a1f246ae9ef1009e988da6
nbrockett <n.brockett@gmail.com>
2020-09-10 22:03:42 +0100

    added new ls la aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,12 +1,15 @@
 
 # Listing sorted
 alias dusort='du -a -d 1 -h | sort -h'
+alias ls='ls --almost-all --classify --color=always --group-directories-first --literal'
+alias la='ls -l --almost-all --si'
 
 # Navigation
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
+
 
 
 
